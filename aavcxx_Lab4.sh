80ccd3a1e8b22dce1f823bcb0e0eb125bd5bf0f1
Avery Vaughn <nokizaru_901@protonmail.com>
2022-02-09 20:58:05 -0600

    Again Ultra Return of the Ultra

--- aavcxx_Lab4.sh
+++ aavcxx_Lab4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #a script to create a simple ping sweep of a network space
-#lab 4 for INFOTC-3910 Advnanced Cyber Security/Dr. Ronny Bazan-Antequera (prof)
+#lab 4 for INFOTC-3910 Advanced Cyber Security/Dr. Ronny Bazan-Antequera (prof)
 #keep ciruclating the tapes
 # this script will use the 192.168.200.0/24 network as its search area
 #begin 
