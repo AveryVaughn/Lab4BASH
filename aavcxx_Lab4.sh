91d1f1f6139a00fbe7a81cc3342030b7f562ca50
Avery Vaughn <nokizaru_901@protonmail.com>
2022-02-09 20:47:14 -0600

    again

--- aavcxx_Lab4.sh
+++ aavcxx_Lab4.sh
@@ -4,11 +4,11 @@
 #lab 4 for INFOTC-3910 Advnanced Cyber Secuirty/Dr. Ronny Bazan-Antequera (prof)
 #keep ciruclating the tapes
 # this script will use the 192.168.200.0/24 network as its search area
-$minVal = $1
-$maxVal = $2
+minVal = $1
+maxVal = $2
 #begin 
 echo "lab_4PingSWeeper"
-for i in {$minVal .. $maxVal}
+for i in {minVal .. maxVal}
     do
         echo "192.168.200.$i"
     done
